0f6effb9522b154dc9ff009164d68688973f4228
Aaron Jensen <aaronjensen@gmail.com>
2020-04-11 18:43:08 -0700

    Round trip time is calculated

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import './App.css';
 import {useQuery, useMutation} from 'react-query'
 import {ActionCableProvider, useActionCable} from 'use-action-cable'
 
+let now = performance.now()
+
 const Count = ({id, value}) => {
-  const [increment] = useMutation(() => fetch(`http://localhost:3000/counts/${id}`, {method: 'PUT'}))
+  const [increment] = useMutation(() => {
+    now = performance.now()
+
+    return fetch(`http://localhost:3000/counts/${id}`, {method: 'PUT'})
+  })
   const [decrement] = useMutation(() => fetch(`http://localhost:3000/counts/${id}?decrement=1`, {method: 'PUT'}))
   const [newValue, setNewValue] = useState()
   useActionCable(
     {channel: 'CountChannel', countId: id},
-    {received: data => setNewValue(data.value)}
+    {received: data => {
+      console.log(performance.now() - now)
+      setNewValue(data.value)
+    }}
   )
 
-  return <li>
-    id: {id}, value: {newValue || value}
-    <button type="button" style={{marginLeft: 10}} onClick={decrement}>-</button>
-    <button type="button" style={{marginLeft: 10}} onClick={increment}>+</button>
-  </li>
+  return <tr>
+    <td>
+      {id}
+    </td>
+    <td style={{width: 100, textAlign: 'right'}}>
+      {newValue || value}
+    </td>
+    <td>
+      <button type="button" style={{marginLeft: 10, padding: 10}} onClick={decrement}>-</button>
+      <button type="button" style={{marginLeft: 10, padding: 10}} onClick={increment}>+</button>
+    </td>
+  </tr>
 }
 
 function App() {
@@ -31,9 +47,17 @@ function App() {
 
   return (
     <ActionCableProvider url="http://localhost:3000/cable">
-      <ul>
-        {counts.map(({id, value}) => <Count id={id} value={value} key={id} />)}
-      </ul>
+      <table style={{margin: 20}}>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>count</td>
+          </tr>
+        </thead>
+        <tbody>
+          {counts.map(({id, value}) => <Count id={id} value={value} key={id} />)}
+        </tbody>
+      </table>
     </ActionCableProvider>
   );
 }
